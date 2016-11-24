32e858650e0e4bb7b14a97ed1ba2c7217d878174
Jorge <646122@unizar.es>
2016-11-24 22:27:43 +0100

    First commit

---
+++ GitHubTutorial/src/unizar/eina/ucode/tutorial/Sumadora.java
@@ -0,0 +1,29 @@
+package unizar.eina.ucode.tutorial;
+
+public class Sumadora {	
+
+    /**
+     * Pre: VIVA ESPAÑA
+     * Post: Yo que coño sé
+     */
+    public static int suma () {
+        return 10 + 5 + 10;
+    }
+    
+    /**
+     * Pre: VIVA EL REY
+     * Post: Yo que coño sé
+     */
+    public static int otroMetodo () {
+        return 0;
+    }
+
+    /**
+     *  Pre: ---
+     *  Post: NICOLAS MADURO EN LA CARCEL
+     */
+    public static void main (String[] argumentos) {
+    	System.out.println(suma());
+    }
+
+}
